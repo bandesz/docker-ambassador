dd3587dee49b59102878374c187c62a7ffa897d8
andras.ferencz-szabo <andras.ferencz-szabo@overseasstudentliving.com>
2015-03-10 10:42:20 +0000

    Generate supervisor configs for all ports

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,6 @@ RUN apt-get update -qq -y && \
     apt-get install -qq -y openssl socat supervisor && \
     apt-get clean
 
-ADD /start-socat.sh /start-socat.sh
 ADD /run.sh /run.sh
-
-ADD /supervisord-socat.conf /etc/supervisor/conf.d/socat.conf
-RUN chmod 755 /*.sh
 
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+env | grep _TCP= | while read line; do
+  name=$(echo $line | sed -e 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat_\1/')
+  
+  if [ -z $ENABLE_SSL ] then
+    cmd=$(echo $line | sed -e 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat -ls TCP4-LISTEN:\1,fork,reuseaddr TCP4:\2:\3 /')
+  else
+    echo "$CLIENT_PRIVATE_KEY" > /etc/client.pem
+    echo "$SERVER_PUBLIC_KEY" > /etc/server.crt
+    chmod 600 /etc/client.pem /etc/server.crt
+    if [ -z $ROLE == 'server' ] then
+  	  cmd=$(echo $line | sed -e 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat OPENSSL-LISTEN:\1,fork,reuseaddr,cert=\/etc\/server.pem,cafile=\/etc\/client.crt TCP4:\2:\3 /')
+    else
+      cmd=$(echo $line | sed -e 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat TCP-LISTEN:\1,reuseaddr,fork OPENSSL:\2:\3,cert=\/etc\/client.pem,cafile=\/etc\/server.crt /')
+    fi
+  fi  
+
+  cat <<EOF >> /etc/supervisor/conf.d/supervisor-socat.conf
+[program:$name]
+command=$cmd
+numprocs=1
+autostart=true
+autorestart=true
+EOF
+done
+
 exec supervisord -n
